c99ff76a5330e5e1d7d854bc681d615022a9e559
ztr <2879618953@qq.com>
2022-07-17 17:57:44 +0800

    js准备/

---
+++ vuenotes/myrouter/src/components/detail.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <ul>
+        <!-- <li>编号：{{$route.query.id||$route.params.id}}</li>
+        <li>标题：{{$route.query.title||$route.params.title}}</li> -->
+        
+       
+        <!-- <li v-if="a">a:{{a}}</li>
+        <li v-if="b">b:{{b}}</li> -->
+       <li>编号：{{id}}</li>
+        <li>标题：{{title}}</li>
+        
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'detail',
+    /* props:['a','b'] */
+    props:['id','title']
+}
+</script>
+
+<style>
+
+</style>
